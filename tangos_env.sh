337d2f41bf58274d2dc6f2f8df1ff3517ddd5a53
Michael Tremmel <m.tremmel6@gmail.com>
2023-06-14 17:01:32 +0100

    Update tangos_env.sh

--- tangos_env.sh
+++ tangos_env.sh
@@ -1,7 +1,2 @@
 export TANGOS_PROPERTY_MODULES=tangos_properties_tremmel,tangos_nbodyshop_properties
-export TANGOS_SIMULATION_FOLDER=/nobackupp2/mtremmel/Romulus/
-#export TANGOS_DB_CONNECTION=~/Research/Data/Romulus/h1.cosmo50/Database/data_romulusC.working.db
-#export TANGOS_DB_CONNECTION=~/Research/Data/Romulus/h1.cosmo50/Database/data_romulusC.backup5.db
-#export TANGOS_DB_CONNECTION=~/Research/Data/Romulus/cosmo25/Database/data_romulus25.working.db
-#export TANGOS_DB_CONNECTION=/Volumes/Knowhere/Romulus/cosmo25/data_romulus25.working.db
-export TANGOS_DB_CONNECTION=/Volumes/Knowhere/Romulus/h1.cosmo50/data_romulusC.working.db.backup
+export TANGOS_DB_CONNECTION=/path/to/simulation/database #Change this to your specific path!
