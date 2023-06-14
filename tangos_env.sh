2e2a12059baf1946d851f9df768ffc0c89bad80e
Michael Tremmel <m.tremmel6@gmail.com>
2023-06-14 17:00:27 +0100

    Add files via upload

---
+++ tangos_env.sh
@@ -0,0 +1,7 @@
+export TANGOS_PROPERTY_MODULES=tangos_properties_tremmel,tangos_nbodyshop_properties
+export TANGOS_SIMULATION_FOLDER=/nobackupp2/mtremmel/Romulus/
+#export TANGOS_DB_CONNECTION=~/Research/Data/Romulus/h1.cosmo50/Database/data_romulusC.working.db
+#export TANGOS_DB_CONNECTION=~/Research/Data/Romulus/h1.cosmo50/Database/data_romulusC.backup5.db
+#export TANGOS_DB_CONNECTION=~/Research/Data/Romulus/cosmo25/Database/data_romulus25.working.db
+#export TANGOS_DB_CONNECTION=/Volumes/Knowhere/Romulus/cosmo25/data_romulus25.working.db
+export TANGOS_DB_CONNECTION=/Volumes/Knowhere/Romulus/h1.cosmo50/data_romulusC.working.db.backup
